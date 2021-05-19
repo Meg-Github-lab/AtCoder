792b45d9cdaf72ff114f32eb179af4070b6abe7c
Meg <75002983+Meg-Github-lab@users.noreply.github.com>
2021-05-19 22:30:51 +0900

    2021/05/19

--- ABC/A_code.py
+++ ABC/A_code.py
@@ -1,6 +1,5 @@
-# https://atcoder.jp/contests/abc003/tasks/abc003_1
-
 n = int(input())
-n_list = list(range(1,n+1))
 
-print(int(1/n * sum(n_list) * 10000))
+print("YES") if n%3 == 0 or "3" in str(n) else print("NO")
+
+# https://note.nkmk.me/python-if-conditional-expressions/
